e42b7218b40aabb53adbd63068075348f9a09932
angel <211508797+angela-spirova@users.noreply.github.com>
2026-03-05 19:31:06 +0100

    Create grid

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const root = document.documentElement;
 const container = document.getElementById('container');
+
+function createGrid(gridSize=16){
+    root.style.setProperty('--grid-size', gridSize);
+    const gridCellSideLength = container.style.width / gridSize;
+    for(let i=0; i<gridSize*gridSize; i++){
+        const gridCell = document.createElement('div');
+
+        gridCell.classList.add('grid-cell');
+
+        container.appendChild(gridCell);
+    }
+}
+createGrid();
